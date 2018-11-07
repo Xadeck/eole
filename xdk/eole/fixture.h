9625a260f1672a664889f5632176750945c3c081
Xavier Décoret <xdecoret@xdecoret-macbookair.roam.corp.google.com>
2018-11-07 22:35:19 +0100

    Added simple fixture

---
+++ xdk/eole/fixture.h
@@ -0,0 +1,27 @@
+#ifndef XDK_EOLE_FIXTURE_H
+#define XDK_EOLE_FIXTURE_H
+
+#include <string>
+
+#include "absl/strings/string_view.h"
+
+namespace xdk {
+namespace eole {
+
+class Fixture {
+public:
+  Fixture();
+  const std::string &Dirpath() const { return dirpath_; }
+  std::string Filepath(absl::string_view relative_filepath) const;
+
+  void AddFile(absl::string_view relative_filepath,
+               absl::string_view content) const;
+
+private:
+  std::string dirpath_;
+};
+
+} // namespace eole
+} // namespace xdk
+
+#endif
---
+++ xdk/eole/fixture_test.cc
@@ -0,0 +1,35 @@
+#include "xdk/eole/fixture.h"
+
+#include <fstream>
+
+#include "xdk/eole/path.h"
+#include "gmock/gmock.h"
+#include "gtest/gtest.h"
+
+namespace xdk {
+namespace eole {
+namespace {
+using ::testing::EndsWith;
+
+class FixtureTest : public ::testing::Test {
+public:
+  Fixture fixture_;
+};
+
+TEST_F(FixtureTest, ContainsCurrentTestInfo) {
+  EXPECT_THAT(fixture_.Dirpath(),
+              EndsWith(Path::Join("FixtureTest", "ContainsCurrentTestInfo")));
+}
+
+TEST_F(FixtureTest, CanAddFile) {
+  constexpr char relative_filepath[] = "foo/bar/baz.txt";
+  fixture_.AddFile(relative_filepath, "some content");
+
+  std::ifstream ifs(fixture_.Filepath(relative_filepath));
+  std::string content{std::istreambuf_iterator<char>(ifs),
+                      std::istreambuf_iterator<char>()};
+  EXPECT_EQ(content, "some content");
+}
+} // namespace
+} // namespace eole
+} // namespace xdk
